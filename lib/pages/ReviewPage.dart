ebd7cab19f7b12e04fb6b2b6ba5a3aa9aa08041e
jamesweber7 <jaweber7@asu.edu>
2021-05-06 13:44:42 -0700

    added reviewpage

---
+++ lib/pages/ReviewPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ReviewPage extends StatefulWidget {
+  ReviewPage({Key key}) : super(key: key);
+
+  @override
+  _ReviewPageState createState() => _ReviewPageState();
+}
+
+class _ReviewPageState extends State<ReviewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Review")
+      ),
+      body: Center(
+        
+      ),
+    );
+  }
+}
