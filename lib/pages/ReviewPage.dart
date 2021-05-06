2cc8477327cb86f2475a5c3ab7f49888330ec826
thetravisweber <tbroweber@gmail.com>
2021-05-06 14:54:47 -0700

    got forms being loaded...still need to build drop downs

--- lib/pages/ReviewPage.dart
+++ lib/pages/ReviewPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:virago/classes/Brand.dart';
+import 'package:virago/classes/FormofBirthControl.dart';
+import 'package:virago/classes/Store.dart';
 
 
 class ReviewPage extends StatefulWidget {
@@ -11,16 +14,85 @@ class ReviewPage extends StatefulWidget {
 class _ReviewPageState extends State<ReviewPage>
 {
 
+  List<FormofBirthControl> _forms = [];
+  int _renderKey = 0;
+  bool _fetchingForms = false;
+
+  Future<Null> _getForms(Store store) async {
+    _fetchingForms = true;
+    _forms = await store.getForms();
+    setState(() {
+      _renderKey++;
+    });
+  }
+  
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
+    final _store = Store.of(context);
+    
+    if (!_fetchingForms) {
+      // _getForms(_store);
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Reviews")
       ),
       body: Center(
-        
+        child: Column(
+          children: <Widget>[
+            Text(
+              'hello',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            _buildBrandDropDowns(context, _forms)
+          ]
+        )
       ),
     );
   }
+
+  Widget _buildBrandDropDowns(BuildContext context, List<FormofBirthControl> forms) 
+  {
+    if (forms.isEmpty) {
+      return _buildWaiting(context);
+    }
+
+    List<Widget> formDropWidgets = List<Widget>();
+
+    for (final form in forms) {
+      formDropWidgets.add(_buildFormDropDown(form));
+    }
+
+    return Column(
+      children: formDropWidgets,
+    );
+  }
+
+  Widget _buildFormDropDown(FormofBirthControl form)
+  { 
+    return Text(
+      form.title,
+      style: Theme.of(context).textTheme.headline6,
+    );
+  }
+
+  Widget _buildWaiting(context) {
+
+    AnimationController _animatorController = new AnimationController();
+
+    final _colorTween = _animatorController.drive(
+      ColorTween(
+        begin: Colors.yellow,
+        end: Colors.blue
+      )
+    );
+
+    return CircularProgressIndicator(
+      valueColor: _colorTween
+    );
+
+  }
+
 }
